d87befa01ffd6f68a07b2a020a7866495ff0bc77
Kohmmz <jeremiahkoome1@gmail.com>
2024-12-19 04:25:45 +0300

    spellcheck update

--- GameAction.js
+++ GameAction.js
@@ -4,16 +4,30 @@ document.addEventListener("DOMContentLoaded", () => { //wait for the dom to be l
     const characterguess = document.getElementById("character-guess");
     const stargame = document.getElementById("star-game");
     const correctlyguessedlist = document.getElementById("correctly-guessed-list");
-    const AddtoFavourite = document.getElementById("Add to Favourite");
+    const submit = document.getElementById("submit")
+    const Favourite = document.getElementById("add-to-favourite")
+    // function waits for the game button to be clicked to start
+    gamestartbutton.addEventListener("click", async () => {
+
+    })
+    // the submit button to the fetch url
+    function inputguess(submit, characterguess) {
+
+        return characterguess = submit
+    }
+
 
     //fetch data//
     async function getpokimonchar() {
         try {
-            const response = await fetch("https://pokeapi.co/api/v2/pokemon");//get the data from the api
-            const data = await response.json();//change it to json
-            console.log(data.results);//console.log the date
-            return data.results; // to call the array
-        }
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${inputguess}`); //get the data from the api
+            const data = await response.json(); //change it to json
+            console.log(data.results); //log the array
+            return data.results; // return pokemon arry
+        } catch (error) {
+            console.error("Error fetching Pokémon data:", error)
+        };
+
 
     }
 
